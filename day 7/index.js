9901a970aa68fd2b85f76110ccf8c3a8ea8ada8c
Aathavan G <141554484+aathavan1@users.noreply.github.com>
2024-04-24 08:48:29 +0530

    a

--- day 7/index.js
+++ day 7/index.js
@@ -1,16 +1,23 @@
-const exps = require ('express');
+const express = require ('express');
 const path= require('path');
-const ap=exps();
-ap.use(exps.static(path.join(__dirname,'htm')));
-ap.use(exps.static(path.join(__dirname,'asset')));
-//ap.use(express.static(path.join(__dirname,'asset','be')));
-ap.get('/',(req,res)=>{
+const app=express();
+
+
+app.use(exps.static(path.join(__dirname,'htm')));
+
+
+app.use(exps.static(path.join(__dirname,'asset')));
+
+
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'htm','in.html'));
 });
-ap.get('/about',(req,res)=>{
+
+
+app.get('/about',(req,res)=>{
     res.sendFile(path.join(__dirname,'htm','about.html'));
 })
-ap.listen(5000,()=>
-{
+
+app.listen(5000,()=>{
     console.log("server is running in port 5000");
 });
